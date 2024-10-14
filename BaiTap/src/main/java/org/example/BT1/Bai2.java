9373b0be62873d0a70ea2b32c16d4f720e6c8693
SealCia1123 <maoduchai404@gmail.com>
2024-10-14 10:16:34 +0700

    update Bai2

---
+++ BaiTap/src/main/java/org/example/BT1/Bai2.java
@@ -0,0 +1,29 @@
+package org.example.BT1;
+
+import java.util.Scanner;
+
+public class Bai2 {
+    public static void main(String[] args) {
+        int n;
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Nhap vao n: ");
+        n = sc.nextInt();
+        int[] arr = new int[n + 1];
+        System.out.print("Nhap vao mang: ");
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = sc.nextInt();
+        }
+        System.out.print("Nhap vao x: ");
+        int x = sc.nextInt();
+
+        // [2, 3, 1] = 1x^2 + 3x^1 + 2x^0
+        // [2, 5, 1, 7] = 7x^3 + 1x^2 + 5x^1 + 2x^0
+        int count = n;
+        int result = 0;
+        for (int i = 0; i < arr.length; i++) {
+            result += (int) (arr[i] * Math.pow(x, n - count));
+            count--;
+        }
+        System.out.printf("f(%d) = %d\n", x, result);
+    }
+}
