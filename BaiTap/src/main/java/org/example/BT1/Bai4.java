5961616c53e76f3daf789b9ccec142d6a8f3b3bc
SealCia1123 <maoduchai404@gmail.com>
2024-10-20 11:14:48 +0700

    update Bai4

--- BaiTap/src/main/java/org/example/BT1/Bai4.java
+++ BaiTap/src/main/java/org/example/BT1/Bai4.java
@@ -1,5 +1,7 @@
 package org.example.BT1;
 
+import java.util.Arrays;
+
 public class Bai4 {
     private static boolean isPrime(int x) {
         if (x < 2)
@@ -17,15 +19,103 @@ public class Bai4 {
             if (isPrime(i)) {
                 sum += i;
             }
-
         }
         return sum;
     }
 
+    private static int[] sortPrimeArr(int[] arr) {
+        int[] newArr = Arrays.copyOf(arr, arr.length);
+        int count = 0;
+        for (int i : newArr) {
+            if (isPrime(i))
+                count++;
+        }
+        int[] primeArr = new int[count];
+        int[] posArr = new int[count];
+        int index = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if (isPrime(arr[i])) {
+                primeArr[index] = arr[i];
+                posArr[index++] = i;
+            }
+        }
+
+        for (int i = 0; i < primeArr.length - 1; i++) {
+            for (int j = i + 1; j < primeArr.length; j++) {
+                if (primeArr[i] > primeArr[j]) {
+                    // swap(arr, i, j);
+                    int temp = primeArr[i];
+                    primeArr[i] = primeArr[j];
+                    primeArr[j] = temp;
+                }
+            }
+        }
+
+        for (int i = 0; i < primeArr.length; i++) {
+            newArr[posArr[i]] = primeArr[i];
+        }
+        return newArr;
+    }
+
+    private static void findMinNegativeNumber(int[] arr) {
+        int min = 999999999;
+        for (int i : arr) {
+            if (i < 0 && i < min)
+                min = i;
+        }
+        if (min == 999999999)
+            System.out.println("*");
+        else
+            System.out.println("Min: " + min);
+    }
+
+    private static void findMaxPositiveNumber(int[] arr) {
+        int max = -999999999;
+        for (int i : arr) {
+            if (i > 0 && i > max)
+                max = i;
+        }
+        if (max == -999999999)
+            System.out.println("*");
+        else
+            System.out.println("Max: " + max);
+    }
+
+    private static void findMinPrimeNumber(int[] arr) {
+        int min = 99999999;
+        for (int i : arr) {
+            if (isPrime(i) && i < min)
+                min = i;
+        }
+        if (min == 99999999)
+            System.out.println("*");
+        else
+            System.out.println("Số nguyên tố nhỏ nhất: " + min);
+    }
+
     public static void main(String[] args) {
-        int[] arr = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        int[] arr = {3, 66, 99, 11, 2, 4, 7};
         // Cau a
         System.out.println(sumOfPrimeNumber(arr));
+
         // Cau b
+        findMinNegativeNumber(arr);
+        findMaxPositiveNumber(arr);
+
+        // Cau c
+        findMinPrimeNumber(arr);
+
+        // Cau d
+        int[] b = sortPrimeArr(arr);
+        System.out.println();
+        for (int i : b) {
+            System.out.printf("%d ", i);
+        }
+        System.out.println();
+
+        // Cau e
+        int x = 3;
+        int result = Arrays.binarySearch(arr, x);
+        System.out.println(result);
     }
 }
