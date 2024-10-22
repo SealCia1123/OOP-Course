98c2ff6a3694dbf3fd9ddb91ad7445d93e1456ee
SealCia1123 <maoduchai404@gmail.com>
2024-10-22 13:36:08 +0700

    update

--- BaiTap/src/main/java/org/example/BT1/Bai2.java
+++ BaiTap/src/main/java/org/example/BT1/Bai2.java
@@ -1,29 +1,21 @@
 package org.example.BT1;
 
+import java.util.Map;
 import java.util.Scanner;
 
 public class Bai2 {
     public static void main(String[] args) {
-        int n;
         Scanner sc = new Scanner(System.in);
-        System.out.print("Nhập vào n: ");
-        n = sc.nextInt();
-        int[] arr = new int[n + 1];
-        System.out.print("Nhập vào mang: ");
-        for (int i = 0; i < arr.length; i++) {
-            arr[i] = sc.nextInt();
-        }
-        System.out.print("Nhập vào x: ");
-        int x = sc.nextInt();
-
-        // [2, 3, 1] = 1x^2 + 3x^1 + 2x^0
-        // [2, 5, 1, 7] = 7x^3 + 1x^2 + 5x^1 + 2x^0
-        int count = n;
+        System.out.print("Nhap n: ");
+        int n = Integer.parseInt(sc.nextLine());
+        System.out.print("Nhap he so: ");
+        int x = Integer.parseInt(sc.nextLine());
         int result = 0;
-        for (int i = 0; i < arr.length; i++) {
-            result += (int) (arr[i] * Math.pow(x, n - count));
-            count--;
+        for (int i = 0; i < n + 1; i++) {
+            System.out.print("Nhap vao ");
+            int a = Integer.parseInt(sc.nextLine());
+            result += (int) (a * Math.pow(x, i));
         }
-        System.out.printf("f(%d) = %d\n", x, result);
+        System.out.println("Ket qua: " + result);
     }
 }
